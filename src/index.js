eb068080311d71d5a9c0503d9b1fccc61f2f81cb
diseno1 <diseno@gmail.com>
2026-04-24 15:24:02 -0400

    fix: ruta correcta index backend

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import categoriasRoutes from "./routes/categorias.routes.js";
 import inventarioRoutes from "./routes/inventario.routes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
