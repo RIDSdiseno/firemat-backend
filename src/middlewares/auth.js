e587c1088c4ed8cc567ce5883860bb3111938a30
diseno1 <diseno@gmail.com>
2026-04-29 16:06:38 -0400

    feat: reserva y confirmacion de stock

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,17 +3,26 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  console.log("AUTH HEADER:", authHeader);
+
   if (!authHeader) {
     return res.status(401).json({ message: "No autorizado" });
   }
 
   const token = authHeader.split(" ")[1];
 
+  console.log("TOKEN:", token);
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("DECODED:", decoded);
+
     req.user = decoded;
     next();
   } catch (err) {
+    console.error("ERROR JWT:", err.message);
+
     return res.status(403).json({ message: "Token inválido" });
   }
 };
